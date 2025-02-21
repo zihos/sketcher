f0e7526bac6b4d732c7f68fffdb65dd2421867f7
zihos <jiho8345@naver.com>
2025-02-21 04:27:36 +0000

    update

--- ddpo_pytorch/rewards.py
+++ ddpo_pytorch/rewards.py
@@ -115,6 +115,71 @@ def llava_strict_satisfaction():
     return _fn
 
 
+def mplug_tifascore():
+    """Submits images to LLaVA and computes a reward by comparing the responses to the prompts using BERTScore. See
+    https://github.com/kvablack/LLaVA-server for server-side code.
+    """
+    import requests
+    from requests.adapters import HTTPAdapter, Retry
+    from io import BytesIO
+    import pickle
+
+    batch_size = 1
+    url = "http://127.0.0.1:8085"
+    sess = requests.Session()
+    retries = Retry(
+        total=1000, backoff_factor=1, status_forcelist=[500], allowed_methods=False
+    )
+    sess.mount("http://", HTTPAdapter(max_retries=retries))
+
+    def _fn(images, prompts, metadata):
+        del metadata
+        if isinstance(images, torch.Tensor):
+            images = (images * 255).round().clamp(0, 255).to(torch.uint8).cpu().numpy()
+            images = images.transpose(0, 2, 3, 1)  # NCHW -> NHWC
+
+        images_batched = np.array_split(images, np.ceil(len(images) / batch_size))
+        prompts_batched = np.array_split(prompts, np.ceil(len(prompts) / batch_size))
+
+        all_scores = []
+        
+        for image_batch, prompt_batch in zip(images_batched, prompts_batched):
+            jpeg_images = []
+
+            # Compress the images using JPEG
+            for image in image_batch:
+                img = Image.fromarray(image)
+                buffer = BytesIO()
+                img.save(buffer, format="JPEG", quality=80)
+                jpeg_images.append(buffer.getvalue())
+
+            # format for LLaVA server
+            data = {
+                "images": jpeg_images,
+                "queries": [f"{prompt_batch[0]}"]
+                * len(image_batch)
+            }
+            data_bytes = pickle.dumps(data)
+
+            # send a request to the llava server
+            response = sess.post(url, data=data_bytes, timeout=120)
+
+            response_data = pickle.loads(response.content)
+
+            # use the tifa score as the reward
+            all_scores.append(response_data["outputs"])
+
+            # save the precision and f1 scores for analysis
+            # all_info["precision"] += (
+            #     np.array(response_data["precision"]).squeeze().tolist()
+            # )
+            # all_info["f1"] += np.array(response_data["f1"]).squeeze().tolist()
+            # all_info["outputs"] += np.array(response_data["outputs"]).squeeze().tolist()
+
+        return np.array(all_scores), np.array(all_scores)
+
+    return _fn
+
 def llava_bertscore():
     """Submits images to LLaVA and computes a reward by comparing the responses to the prompts using BERTScore. See
     https://github.com/kvablack/LLaVA-server for server-side code.
@@ -125,7 +190,7 @@ def llava_bertscore():
     import pickle
 
     batch_size = 16
-    url = "http://127.0.0.1:8085"
+    url = "http://127.0.0.1:8087"
     sess = requests.Session()
     retries = Retry(
         total=1000, backoff_factor=1, status_forcelist=[500], allowed_methods=False
@@ -175,15 +240,15 @@ def llava_bertscore():
 
             # use the recall score as the reward
             scores = np.array(response_data["recall"]).squeeze()
-            all_scores += scores.tolist()
+            all_scores.append(scores)
 
             # save the precision and f1 scores for analysis
             all_info["precision"] += (
-                np.array(response_data["precision"]).squeeze().tolist()
+                [np.array(response_data["precision"]).squeeze()]
             )
-            all_info["f1"] += np.array(response_data["f1"]).squeeze().tolist()
-            all_info["outputs"] += np.array(response_data["outputs"]).squeeze().tolist()
-
+            all_info["f1"] += [np.array(response_data["f1"]).squeeze()]
+            all_info["outputs"] += [np.array(response_data["outputs"]).squeeze()]
+            
         return np.array(all_scores), {k: np.array(v) for k, v in all_info.items()}
 
     return _fn
